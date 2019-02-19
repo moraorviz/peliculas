9d4a3aff6e47f970ad55dc31d2f3d5855380f872
Mario Moraño Orviz <moraorviz@gmail.com>
2019-02-19 20:41:03 +0100

    modificacion comparador

--- comparador/src/main/java/com/miw/peliculas/webservices/ComparadorWS.java
+++ comparador/src/main/java/com/miw/peliculas/webservices/ComparadorWS.java
@@ -16,41 +16,23 @@ public class ComparadorWS implements IComparadorWS {
 		NETFLIX, AMAZON, CARREFOUR, ALCAMPO, TUVIDEOCLUB, FNAC, MEDIAMARKT
 	}
 
-	public ComparadorWS() {
-		peliculas = new ArrayList<Pelicula>();
-		peliculas.add(new Pelicula("001", "El silencio de los corderos"));
-		peliculas.add(new Pelicula("002", "Star Wars III"));
-		peliculas.add(new Pelicula("003", "Rocky"));
-		peliculas.add(new Pelicula("004", "Rocky II"));
-		peliculas.add(new Pelicula("005", "El señor de los anillos"));
-		peliculas.add(new Pelicula("006", "El señor de los anillos I"));
-		peliculas.add(new Pelicula("007", "El señor de los anillos II"));
-		peliculas.add(new Pelicula("008", "El señor de los anillos III"));
-	}
-
 	public Comparacion[] comparar(String titulo) {
-		
+
 		comparaList = new ArrayList<Comparacion>();
 
-		for (int i = 0; i < peliculas.size(); i++) {
-			Pelicula temp = peliculas.get(i);
-			if ((temp.getTitulo()).equals(titulo)) {
-				for (Comercio c : Comercio.values()) {
-					comparaList.add(new Comparacion(c.toString(), calcularPrecio()));
-				}
-				Comparacion[] comparaArray = new Comparacion[comparaList.size()];
-				return comparaList.toArray(comparaArray);
-			}
+		for (Comercio c : Comercio.values()) {
+			comparaList.add(new Comparacion(c.toString(), calcularPrecio()));
 		}
-		return null;
+		Comparacion[] comparaArray = new Comparacion[comparaList.size()];
+		return comparaList.toArray(comparaArray);
 
 	}
 
 	public double calcularPrecio() {
-		
+
 		Random r = new Random();
-        double precio = Math.floor(5 + (15 - 5) * r.nextDouble());
-        return precio;
+		double precio = Math.floor(5 + (15 - 5) * r.nextDouble());
+		return precio;
 	}
 
 }
