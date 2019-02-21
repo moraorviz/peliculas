38d3dca9f5dd8ba2c1bce823d9d4f6463a5c27e8
Mario <mario@fedora-28.shared>
2019-02-22 00:14:11 +0100

    todo

--- cliente-web/src/main/java/com/miw/peliculas/client/ComparadorWSClient.java
+++ cliente-web/src/main/java/com/miw/peliculas/client/ComparadorWSClient.java
@@ -7,7 +7,7 @@ import com.miw.peliculas.webservices.ComparadorWSService;
 import com.miw.peliculas.webservices.IComparadorWS;
 
 // TODO implementar el argumento en la interfaz que permita escoger el tipo 
-// de servicio (más en la documentación de la práctica)
+// de servicio (más en la documentación de la práctica)	
 public class ComparadorWSClient {
 	public static List<Comparacion> comparar() {
 		ComparadorWSService service = new ComparadorWSService();
