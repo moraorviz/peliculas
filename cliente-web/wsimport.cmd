2e9334e040a80941a989feb107bc896e207876b1
Mario Moraño Orviz <moraorviz@gmail.com>
2019-02-21 17:08:41 +0100

    added wsimport script

---
+++ cliente-web/wsimport.cmd
@@ -0,0 +1,1 @@
+"%JAVA_HOME%"\bin\java.exe -Djavax.xml.accessExternalSchema=all -jar .\tools\wsimport\jaxws-tools.jar -p com.miw.peliculas.webservices -Xnocompile http://localhost:8080/comparador/soapws/comparador?wsdl
