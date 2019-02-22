70f6e43d9d1b01b15b26dca9e9b38e2be845ec0c
Mario Moraño Orviz <moraorviz@gmail.com>
2019-02-22 08:52:09 +0100

    added wsimport command for windows cmd

---
+++ cliente-web/wsimport.cmd
@@ -0,0 +1,1 @@
+"%JAVA_HOME%\bin\java.exe" -Djavax.xml.accessExternalSchema=all -jar .\tools\wsimport\jaxws-tools.jar -Xnocompile -p com.miw.peliculas.webservices http://localhost:8080/comparador/soapws/comparador?wsdl
